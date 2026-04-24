b8cefe46fc315548e54276574c626554e630a493
jsfindo <johannes.sivola@gmail.com>
2026-04-24 14:34:02 +0300

    all done

--- scripts.js
+++ scripts.js
@@ -1,16 +1,56 @@
 const container = document.createElement("div")
-document.append(container)
-
-const div = document.createElement("div")
-function grid(){
-    for(i=0; i<(16*16-1); i++)
-        document.container.appendChild(div)
-}
-
-grid
+document.body.append(container)
 
 container.style.display = "flex"
 container.style.flexWrap = "wrap"
+container.style.width = "960px";
+container.style.height = "960px";
+container.style.border = "2px solid red";
 
-div.style.flex = "0 0 6.25%"
-div.style.backgroundColor = "blue"
+const reset = document.createElement("button")
+reset.innerText ="RESET"
+document.body.append(reset)
+
+
+ 
+let gridSize = prompt("how many boxes per row should the grid have?")
+if(gridSize > 100){
+    let gridSize = prompt("how many boxes per row should the grid have? Please input a value less than 100 ;)")
+}
+ 
+
+
+
+ let squareSize = 100/gridSize
+
+ 
+    for(i=0; i<(gridSize * gridSize); i++){
+        const div = document.createElement("div")
+        
+        div.style.flex = `0 0 ${squareSize}%`
+        div.style.backgroundColor = "lightblue"
+        div.style.border = "0.1px solid black";
+        div.style.height = `${squareSize}%`;
+        div.style.boxSizing ="border-box"
+
+        div.addEventListener( 'mouseenter',(e)=>{
+            div.style.backgroundColor = "darkblue"
+        })
+
+        div.addEventListener( 'mouseleave',(e)=>{
+            div.style.backgroundColor = "blue"
+        })
+
+        reset.addEventListener("click", (e)=>{
+        div.style.backgroundColor = "lightblue"
+        })
+        
+        container.append(div)
+    }
+        
+
+
+
+
+
+
